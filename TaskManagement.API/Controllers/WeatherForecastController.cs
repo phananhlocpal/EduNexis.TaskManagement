358420149e1ab7a399fcc09b62d06faa94cd983c
phananhlocpal <anhloc280@gmail.com>
2025-09-28 15:04:21 +0700

    f

--- TaskManagement.API/Controllers/WeatherForecastController.cs
+++ TaskManagement.API/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace TaskManagement.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatffffffherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
